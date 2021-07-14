3f057759176dd286f518f50fe14a5feca3f79082
David Orchard <if_coding@fastmail.com>
2021-07-14 04:51:24 -0500

    Automatically un/install aliases

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+#[macro_use] extern crate custom_derive;
+#[macro_use] extern crate enum_derive;
+
 mod wrap;
+mod shell;
 
 use anyhow::{Context, Result, bail};
 use std::path::PathBuf;
@@ -13,13 +17,24 @@ use wrap::Config;
     setting = structopt::clap::AppSettings::TrailingVarArg,
 )]
 struct Args {
+    /// Install aliases for the current shell
+    ///
+    /// Currently supported shells:
+    ///   - fish
+    #[structopt(long)]
+    alias: bool,
+
     /// The file to publish to rabbit
     #[structopt(env, short, long)]
     config: Option<PathBuf>,
 
     /// Print the command to be run
-    #[structopt(short = "n", long)]
+    #[structopt(short = "n", long, conflicts_with_all = &["alias", "unalias"])]
     dry_run: bool,
+
+    /// Uninstall aliases for the current shell
+    #[structopt(long, conflicts_with = "alias")]
+    unalias: bool,
 
     /// Positional arguments to pass to the underlying command
     args: Vec<String>,
@@ -33,6 +48,21 @@ fn main(mut args: Args) -> Result<()> {
     pretty_env_logger::init();
 
     let config = Config::new(&args.config)?;
+
+    if args.alias {
+        let aliases = config.aliases.get_aliases();
+        shell::alias(&aliases)?;
+        println!("Shell aliases installed. You may need to restart your shell session to take effect");
+        return Ok(());
+    }
+
+    if args.unalias {
+        let aliases = config.aliases.get_aliases();
+        shell::unalias(&aliases)?;
+        println!("Shell aliases uninstalled. You may need to restart your shell session to take effect");
+        return Ok(());
+    }
+
     if args.args.is_empty() {
         bail!("No alias given");
     }
@@ -41,10 +71,7 @@ fn main(mut args: Args) -> Result<()> {
     let dry_run = global_dry_run(&mut args.args) || args.dry_run;
 
     let user_alias = args.args.remove(0);
-    let alias = config.aliases
-        .into_iter()
-        .find(|alias| alias == &user_alias)
-        .context("No matching alias found")?;
+    let alias = config.aliases.get_alias(&user_alias).context("No matching alias found")?;
     let command = alias.get_command(args.args);
 
     if dry_run {
--- src/wrap/alias.rs
+++ src/wrap/alias.rs
@@ -6,8 +6,9 @@ use super::{command::Command, default_argument::DefaultArguments, keyword::Keywo
 pub struct Alias {
     pub alias: String,
     pub program: String,
-    #[serde(rename = "arguments")]
+    #[serde(rename = "arguments", default)]
     pub default_arguments: DefaultArguments,
+    #[serde(default)]
     pub keywords: Keywords,
 }
 
@@ -27,5 +28,23 @@ impl Alias {
             program: self.program,
             arguments,
         }
+    }
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub struct Aliases(Vec<Alias>);
+
+impl Aliases {
+    pub fn get_alias(self, alias: &str) -> Option<Alias> {
+        self.0
+            .into_iter()
+            .find(|a| a == &alias)
+    }
+
+    pub fn get_aliases(self) -> Vec<String> {
+        self.0
+            .into_iter()
+            .map(|alias| alias.alias)
+            .collect()
     }
 }
