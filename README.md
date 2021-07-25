717a0b0b97c4a990c1802f1a347f0058c77fec2b
David Orchard <if_coding@fastmail.com>
2021-07-25 12:48:31 -0700

    Use pest instead of custom statemachine parser

--- README.md
+++ README.md
@@ -1,36 +1,79 @@
 # wrap
-A small command to wrap or "bookmark" other commands. Like `alias`, but better
+A small utility to wrap other commands. Like `alias`, but better.
+
+Unlike `alias`, it supports:
+- default arguments (which can be overridden at runtime)
+- argument aliases
+- global dry-run (no more explanations of "I aliases `k` to `kubectl`" when sharing command snippets!)
 
 # Usage
-Given the following file in `$HOME/.config/wrap.yaml` (other formats are also supported):
+## Configuration
+Wrap uses config files to define aliases. Multiple formats are supported, including `yaml`, `toml`, and `json`.
+`yaml` is the recommended format and will be used in all examples. `wrap` reads its configuration in two ways:
+1. By default, all config locations are read and marged, with more specific configurations overridding values
+   in less specific configurations. Config files can be specified in the following locations and merged in order:
+   - Global config: `/etc/wrap.yaml`
+   - User config: `~/.config/wrap.yaml`
+   - Local config: `./wrap.yaml`
+2. If a config file is specified at runtime with `--config <file>`, then the following configs are read and merged:
+   - Global config: `/etc/wrap.yaml`
+   - Argument-specified file: `<file>`
+
+Here's an example config (which will be used in the rest of the README):
 ```yaml
-wrappers:
-  Run a shell command:
-    trigger: ["sh"]
-    command: sh
-    args:
-      - -c
-  Echo something:
-    trigger: ["echo"]
-    command: echo
-    args: []
+variables:
+  KUBE_DIR: ~/.kube
+  # Variables can contain previously defined variables
+  PROD_KUBECONFIG: $KUBE_DIR/prod
+  # Or environment variables
+  CONFIG_DIR: $HOME/.config
+
+aliases:
+  - alias: k
+    program: kubectl
+
+    # These are the default arguments
+    # Arguments & values can be overridden when invoked
+    arguments:
+      - key: -n  # Required
+        value: default-namespace  # Optional
+        cleared-by: [-n, -A]  # If -n or -A is specified, the default argument is ignored
+
+    # Argument `keywords`. If the keyword is found after the alias,
+    # it will be replaced with one or more argument replacements.
+    # This is not recursively executed
+    keywords:
+        # The first argument matching a key will be replaced.
+        # After the first match, these keys will be completely ignored
+      - keys: [--dev, --development]
+        # The key will be replaced with these values where the key was found
+        values: [--context, my_development_context]
+
+      - keys: [--prod]
+        values: [--kubeconfig, $PROD_KUBECONFIG]
+
+      - keys: [--kafka]
+        values: [-n, kafka-namespace]
 ```
 
-`wrap` has the following behavior:
+## Running
+Given the config file above, here are some common usage patterns:
 ```sh
-# Trigger "Echo something"
-$ wrap echo hi
-hi
+# Run wrap directly, printing the resulting command instead of executing it
+$ wrap k --prod --dry-run
+kubectl "-n" "default-namespace" "--kubeconfig" "/home/user/.kube/prod"
 
-# Trigger "Run a shell command"
-$ wrap sh -c "echo hi"
-hi
+# Install aliases (requires a shell restart)
+$ wrap --alias
 
-# Missing trigger
-$ wrap -n ls .
-Error: No trigger found: ["ls", "."]
+# Now we can invoke aliases directly
+$ k --dev -n my-namespace --dry-run
+kubectl "--context" "my_development_context" "-n" "my-namespace"
 
-# Dry-run
-$ wrap -n echo hi
-echo "hi"
+# Override variables at runtime
+$ KUBE_DIR=/root/.kube k --prod --dry-run
+kubectl "-n" "default-namespace" "--kubeconfig" "/root/.kube/prod"
+
+# Uninstall aliases (requires a shell restart)
+$ wrap --unalias
 ```
--- src/wrap/variable.rs
+++ src/wrap/variable.rs
@@ -1,6 +1,53 @@
+use pest::Parser;
 use serde_derive::Deserialize;
 use std::{collections::HashMap, convert::TryFrom, env};
-use anyhow::{bail, Result};
+use anyhow::{Context, Result};
+//use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "wrap/variable.pest"]
+pub struct ArgumentParser;
+
+impl ArgumentParser {
+    // TODO: return Cow
+    pub fn expand(argument: &str, variables: &HashMap<String, String>) -> Result<String> {
+        // Parse the argument
+        let parsed = ArgumentParser::parse(Rule::argument, argument)
+            .with_context(|| "Argument parsing failed")?
+            .next()
+            .expect("Argument parsing will always produce a token")
+            .into_inner();
+
+        // Expand the argument's variables
+        let mut argument = String::new();
+        for pair in parsed {
+            match pair.as_rule() {
+                Rule::literal => argument.push_str(pair.as_str()),
+                Rule::tilde => argument.push_str(Self::get_var("HOME", variables)),
+                Rule::variable => {
+                    for pair in pair.into_inner() {
+                        match pair.as_rule() {
+                            Rule::variable_ident => argument.push_str(Self::get_var(pair.as_str(), variables)),
+                            _ => {},
+                        }
+                    }
+                }
+                _ => {},
+            };
+        }
+
+        Ok(argument)
+    }
+
+    fn get_var<'a>(name: &str, variables: &'a HashMap<String, String>) -> &'a str {
+        variables
+            .get(name)
+            .map(|s| s.as_str())
+            .unwrap_or_default()
+    }
+
+}
 
 #[derive(Debug, Default, Deserialize)]
 #[serde(try_from = "HashMap<String, String>")]
@@ -17,105 +64,11 @@ impl TryFrom<HashMap<String, String>> for Variables {
         for (key, value) in config_variables {
             if !variables.contains_key(&key) {
                 // Expand variables as we insert them
-                let expended_value = CharacterMode::parse(&value, |var|
-                    variables.get(var)
-                        .map(|s| s.as_str())
-                        .unwrap_or_default()
-                )?;
-                variables.insert(key, expended_value);
+                variables.insert(key, ArgumentParser::expand(&value, &variables)?);
             }
         }
 
         Ok(Variables(variables))
-    }
-}
-
-enum CharacterMode {
-    Literal,
-    Escape,
-    VariableStart,
-    VariableMiddle(bool, String), // has braces, variable name
-    VariableEnd(String), // variable name
-}
-
-enum CharType {
-    Escape,
-    Tilde,
-    VariableStart,
-    StartingBrace,
-    EndingBrace,
-    Literal,
-}
-
-impl CharType {
-    pub fn is_variable(c: char) -> bool {
-        c.is_alphanumeric() || c == '_'
-    }
-}
-
-impl From<char> for CharType {
-    fn from(c: char) -> CharType {
-        use CharType::*;
-
-        match c {
-            '\\' => Escape,
-            '~' => Tilde,
-            '$' => VariableStart,
-            '{' => StartingBrace,
-            '}' => EndingBrace,
-            _ => Literal,
-        }
-    }
-}
-
-impl CharacterMode {
-    fn parse<'a>(input: &str, get_var: impl Fn(&str) -> &'a str) -> Result<String> {
-        use CharacterMode as M;
-        use CharType::*;
-
-        let mut output = String::with_capacity(input.len());
-        let mut state = M::Literal;
-        for c in input.chars() {
-            // Move to next state
-            state = match (state, CharType::from(c)) {
-                (M::Escape, Escape | Tilde | VariableStart) => M::Literal,
-                (M::Escape, _) => bail!("Not a valid escape sequence: {}", c),
-
-                (M::VariableStart, StartingBrace) => M::VariableMiddle(true, String::new()),
-                (M::VariableStart, Literal) if !CharType::is_variable(c) => { bail!("Alphanumeric variable or starting brace expected: {}", c) },
-                (M::VariableStart, Literal) => M::VariableMiddle(false, c.into()),
-                (M::VariableStart, _) => bail!("Only `{` or an alphanumeric character allowed after $"),
-
-                (M::VariableMiddle(true, s), EndingBrace) => M::VariableEnd(s),
-                (M::VariableMiddle(true, s), Literal) if !CharType::is_variable(c) => { bail!("Alphanumeric variable name or ending brace expected for variable {}. Found: {}", s, c) },
-                (M::VariableMiddle(false, s), Literal) if !CharType::is_variable(c) => M::VariableEnd(s),
-                (M::VariableMiddle(b, mut s), Literal) => { s.push(c); M::VariableMiddle(b, s) },
-
-                (_, Escape) => M::Escape,
-                (_, Tilde) => M::VariableEnd("HOME".to_string()),
-                (_, VariableStart) => M::VariableStart,
-                _ => { M::Literal },
-            };
-
-            // Do action
-            match &state {
-                M::Literal => output.push(c),
-                M::VariableEnd(s) => {
-                    output.push_str(&get_var(s))
-                },
-                _ => {},
-            }
-        }
-
-        match &state {
-            M::Escape => bail!("Cannot end in the escape character"),
-            M::VariableStart => bail!("No variable name given"),
-            M::VariableMiddle(true, s) => bail!("No ending brace for variable: {}", s),
-            M::VariableMiddle(false, s) => output.push_str(&get_var(s)),
-            _ => {},
-        };
-
-        Ok(output)
     }
 }
 
@@ -124,12 +77,7 @@ impl Variables {
         let mut output = Vec::with_capacity(arguments.len());
 
         for argument in arguments {
-            output.push(CharacterMode::parse(argument, |var| 
-                self.0
-                    .get(var)
-                    .map(|s| s.as_str())
-                    .unwrap_or_default()
-            )?);
+            output.push(ArgumentParser::expand(&argument, &self.0)?);
         }
 
         Ok(output)
@@ -139,28 +87,34 @@ impl Variables {
 #[cfg(test)]
 mod test {
     use super::*;
-    use anyhow::Result;
 
-    fn get_var<'a>(var: &str) -> &'a str {
-        match var {
-            "VAR1" => "var1_output",
-            _ => "",
-        }
+    fn vars() -> HashMap<String, String> {
+        let mut map = HashMap::new();
+        map.insert("VAR1".to_string(), "var1_output".to_string());
+        map
     }
 
     #[test]
-    fn character_mode_parse_literal_input_literal_output() {
-        let input = "nothing special here";
+    fn argument_parse_empty_input_empty_output() {
+        let input = "";
         let expected = input.to_owned();
-        let actual = CharacterMode::parse(input, get_var).unwrap();
+        let actual = ArgumentParser::expand(input, &vars()).unwrap();
         assert_eq!(expected, actual);
     }
 
     #[test]
-    fn character_mode_parse_variable_input_replaced_output() {
-        let input = "$VAR1";
+    fn argument_parse_literal_input_literal_output() {
+        let input = "nothing special here";
+        let expected = input.to_owned();
+        let actual = ArgumentParser::expand(input, &vars()).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn argument_parse_variable_input_replaced_output() {
+        let input = "${VAR1}";
         let expected = "var1_output";
-        let actual = CharacterMode::parse(input, get_var).unwrap();
+        let actual = ArgumentParser::expand(input, &vars()).unwrap();
         assert_eq!(expected, actual);
     }
 }
